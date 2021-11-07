2449f767caef85f1b5ed034fea6fa200b166266c
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2021-11-06 21:24:09 -0700

    lint and build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import Head from "next/head"
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,11 +8,14 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,500;1,700&family=Varela+Round&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,500;1,700&family=Varela+Round&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
